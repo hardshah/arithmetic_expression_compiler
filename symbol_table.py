bd715be0c973c333cca875142b08e8666823cb3b
Hard Shah <hardshah3@gmail.com>
2024-01-25 16:49:38 -0700

    added symbol table class without methods

---
+++ symbol_table.py
@@ -0,0 +1,14 @@
+
+class SymbolTable:
+    def __init__(self):
+    
+    def add(self, name, info):
+    
+    def lookup(self, name):
+        
+    def within_scope(self):
+
+    def exit_scope(self):
+
+        
+        
