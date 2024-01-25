d8fcaebe063fcb07da353822400283b9b35d903c
Hard Shah <hardshah3@gmail.com>
2024-01-25 16:52:24 -0700

    using dictionary as data struct in symbol table

--- symbol_table.py
+++ symbol_table.py
@@ -1,11 +1,18 @@
 class Symbol:
-    def __init__(self, name, typ)
+    def __init__(self, name, type = None):
+        self.name = name
+        self.type = type
 class SymbolTable:
     def __init__(self):
+        self.symbols = {}
+        self.parent = None
+
     
-    def add(self, name, info):
+    def add(self, symbol):
+            self.symbols[symbol.name] = symbol
     
     def lookup(self, name):
+    
         
     def within_scope(self):
 
