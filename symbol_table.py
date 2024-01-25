dde6147e8cc6d13e4eaf810f5f58ecbc23ad388d
Hard Shah <hardshah3@gmail.com>
2024-01-25 16:54:32 -0700

    symbol table methods implemented

--- symbol_table.py
+++ symbol_table.py
@@ -7,16 +7,19 @@ class SymbolTable:
         self.symbols = {}
         self.parent = None
 
-    
     def add(self, symbol):
             self.symbols[symbol.name] = symbol
     
     def lookup(self, name):
+         if name in self.symbols:
+              return self.symbols
     
-        
-    def within_scope(self):
+    def within_scope(self): #Create a new symbol table and set this symbol table as parent
+         scope = SymbolTable()
+         scope.parent = self 
 
     def exit_scope(self):
+         return self.parent #Go back to parent symbol table
 
         
         
